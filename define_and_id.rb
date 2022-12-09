215f5a9ce431343b5597323c8a40702ad24e8e9a
Andrew Bingham <andrewgbingham@gmail.com>
2022-12-09 13:26:12 -0700

    complete define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,23 @@
 
 # YOU DO the rest:
 
-
+#The include question mark method is being called on the string object "Hello World"
+#The argument "Hello" is being looked for within the string object "Hello World" returning an implicit "true"
 "Hello World".include?("Hello")
 
-
+# The end with question mark method is being called on the string object "Hello World"
+# The argument "Hello" is being searched for at the end of the string object which returns an implicit "false"
 "Hello World".end_with?("Hello")
 
-
+# The end with question mark method is called on string object "Hello World" searching that the string ends with "irl" per the argument
+# The implicit return value is "true" 
 "Hello World".end_with?("rld")
 
-
+# The even question mark method is being called on the integer object "12" which is seeing if the integer in question is divisable by 2
+# The return value is "true"
 12.even?
 
-
+# The next method is being called on integer object "18" which will return the integer value "19" since that is next sequential number
 18.next
 
 
